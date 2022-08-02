abdb506fa7a9052c97d14746ab5cb4dbbef1a8fa
Nicolae Stihhi <nicolae.stihhi@gmail.com>
2022-08-02 21:18:44 +0300

    Better, but way off

--- main.py
+++ main.py
@@ -61,6 +61,10 @@ class PID:
         integral = self.cumulative_moving_average * self.ki
         derivative = rate_of_change * self.kd
 
+        # log('proportional', error)
+        # log('integral    ', self.cumulative_moving_average)
+        # log('derivative  ', rate_of_change)
+
         self.prev_error = error
         delta_vector = (proportional + integral + derivative) * -1
         return delta_vector
@@ -99,7 +103,7 @@ class Rover:
             diff[0] = end_diff if end_diff < 0 else 0
         return tuple(diff)
 
-    def dist_to_target_speed(self, v_final: Dir = (0, 0)):
+    def dist_to_reach_speed(self, v_final: Dir):
         # t = (vf - vi) / a
         # dist = vi * t + 1/2 a * t^2
 
@@ -108,19 +112,29 @@ class Rover:
         v_diff = v_final_np - v_init
         
         # split max acceleration between X and Y to reach v_diff evenly
-        v_theta = math.atan2(*np.flip(v_diff))
-        max_vertical_acc = mars_g if v_theta < 0 else 4 - mars_g
-        acc = np.array((math.cos(v_theta), math.sin(v_theta))) * (4, max_vertical_acc)
-        
-        log('v_final_np', v_final_np)
-        log('v_init', v_init)
-        log('v_diff', v_diff)
-        log('v_theta', math.degrees(v_theta))
-        log('acc', acc)
+        acc_theta = math.atan2(*np.flip(v_diff))
+        max_vertical_acc = mars_g if acc_theta < 0 else 4 - mars_g
+        acc = np.array((math.cos(acc_theta), math.sin(acc_theta))) * (4, max_vertical_acc)
         
         acc_no_zero = np.where(acc != 0, acc, 1)
         time = v_diff / acc_no_zero
+
+        # it takes 6 turns to start accelerating up if shuttle is lateral
+        # + account for stopping the current acceleration
+        time += 6
+
+        # log('v_init', v_init)
+        # log('v_diff', v_diff)
+        log('acc_theta   ', acc_theta)
+        # log('time  ', time)
+        
         dist = v_init * time + 1/2 * acc * time ** 2
+
+        # V_init_y = -22 * 116 + 1/2 * 0.2 * 116^2
+
+        # distanta in care Vx si Vy ajung la 0 in acelasi timp
+        # log('dist  ', dist)
+        log('')
         return dist
 
     def turns_to_plain(self, dist: Tuple[int, int]) -> Tuple[int, int]:
@@ -145,10 +159,16 @@ mars_g = 3.711
 
 n = int(input())  # the number of points used to draw the surface of Mars.
 planet: Planet = Planet(n)
-target_vector_pid = PID((1, 0.7, 0.3))
 
 # PID based on distance from target and landing offset
-distance_pid = PID((1.2, 0.9, 0.3))
+
+#########
+Kp = 1 ##
+Ki = 0 ##
+Kd = 0 ##
+#########
+
+distance_pid = PID((Kp, Ki, Kd))
 distance_pid.set_target((0, 0)) # target is to reach (0, 0) offset to LZ (e.g. land)
 
 for i in range(n):
@@ -171,42 +191,62 @@ while True:
     me = Rover(planet, x, y, hs, vs, f, r, p)
 
     # how far is LZ
-    dist = me.dist_to_target_speed((20, -20))
-    lz_landing_offset = dist + me.loc - me.planet.plain_mid    
-    target_delta = distance_pid.make_iteration(lz_landing_offset)
+    dist = me.dist_to_reach_speed((0, 0))
+
+    # unde ajung la V = (0, 0) daca pun frana cu toata puterea:
+    projected_stopping_location = dist + me.loc
+    # log('projected_stopping_location', projected_stopping_location)
+    # log('')
     
-    log('lz_landing_offset', lz_landing_offset)
+    # dorinta mea: sa ajung la LZ
+    # in ce directie ar trebui sa ma deplasez?
+    road_to_lz = np.array(me.planet.plain_mid) - projected_stopping_location
+
+    # PID determines how much should we correct for error
+    PID_adjust_to_lz = distance_pid.make_iteration(road_to_lz)
+
+    # direction of acceleration considering gravity
+    turns_for_gravity_to_act = 1
+    mars_acceleration_vector = np.array((0, -mars_g)) * turns_for_gravity_to_act
+    target_acceleration = (PID_adjust_to_lz - mars_acceleration_vector) * -1
+
+    # shuttle rotation based on acceleration vector direction
+    acc_rho = vector_rho(target_acceleration)
+    log('acc_rho:', acc_rho)
+    shuttle_rotation = 0
+    if acc_rho > 0:
+        # cadran 1 sau 2
+        # directia de accelerare e in SUS
+        shuttle_rotation = math.degrees(acc_rho) - 90
+    else:
+        # cadran 3 sau 4
+        # las gravitatea sa actioneze; throttle doar spre lateral
+        target_acceleration[1] = 0 
+
+        if acc_rho < - math.pi / 2:
+            # cadran 3
+            shuttle_rotation = 90 # inclinatie maxima spre stanga
+        else:
+            # cadran 4
+            shuttle_rotation = -90 # inclinatie maxima spre stanga
     
-    # direction of thrust considering gravity
-    turns_for_gravity_to_act = 5
-    mars_gravity_vector = np.array((0, -mars_g)) * turns_for_gravity_to_act
-    target_acceleration = (target_delta - mars_gravity_vector) * -1
-
-    # shuttle rotation based on thrust vector
-    acceleration_rad = vector_rho(target_acceleration)
-    magnitude_multiplier = -1
-    if acceleration_rad > 0:
-        acceleration_rad = acceleration_rad - math.pi
-        magnitude_multiplier = magnitude_multiplier * -1
-    acceleration_rad += math.pi / 2
-    acceleration_rad = -acceleration_rad
-
-    thrust_deg = round(math.degrees(acceleration_rad))
-
+    shuttle_rotation = round(shuttle_rotation)
+    
     # thrust vector to magnitude
-    thrust_mag = magnitude(target_acceleration) * magnitude_multiplier
-    if (thrust_mag < 2): thrust_mag = 3
-    if (thrust_mag > 4): thrust_mag = 4
-    thrust_mag = round(thrust_mag)
+    thrust = magnitude(target_acceleration)
+    if (thrust < 0): thrust = 0
+    if (thrust > 4): thrust = 4
+    thrust = round(thrust)
 
-    log('delta - vect:', target_delta)
-    log('accel - vect:', target_acceleration)
-    log('acc - magn:', magnitude(target_acceleration))
-    log('acc - grad:', math.degrees(vector_rho(target_acceleration)))
+    # log('ROAD TO LZ:', road_to_lz)
+    # log('ADJUST    :', PID_adjust_to_lz)
+    # log('acc - magn:', magnitude(target_acceleration))
+    # log('acc - grad:', math.degrees(vector_rho(target_acceleration)))
     # log('thrust_deg', thrust_deg)
     
     
     # R P. R is the desired rotation angle. P is the desired thrust power.
-    out = f"{thrust_deg} {thrust_mag}"
-    log('out', out)
+    out = f"{shuttle_rotation} {thrust}"
+    # log('now', f"{me.rot} {me.power}")
+    # log('out', out)
     print(out)
