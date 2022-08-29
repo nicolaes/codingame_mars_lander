f4df499e29894ea370e77a446770ab15996a8630
Nicolae Stihhi <nicolae.stihhi@gmail.com>
2022-08-30 01:18:37 +0300

    finished basic GA; added timing comments

--- genetic_stub.py
+++ genetic_stub.py
@@ -61,8 +61,11 @@ def is_rover_on_surface(rover: Rover, surface: Surface):
     m, b = get_surface_line_below_rover(rover, surface)
     return rover.y <= (m * rover.x + b)
 
-def simulate_trajectory(rover: Rover, surface: Surface, trajectory: list[int]) \
-        -> Rover:
+def simulate_trajectory(rover: Rover, surface: Surface, trajectory: List[int]) -> Rover:
+    # start_sim = datetime.now().microsecond
+    # initialized_times = False
+    # total_times = None
+
     i = 0
     prev_r = rover
     r = rover
@@ -72,19 +75,30 @@ def simulate_trajectory(rover: Rover, surface: Surface, trajectory: list[int]) \
         not (on_surface := is_rover_on_surface(r, surface)) and
         i < len(trajectory)
     ):
+        # times = np.append([], datetime.now().microsecond)
+        
         # Next power based on trajectory
         rotate = np.clip(r.rotate + trajectory[i], -90, 90)
+        # try:
         power = np.clip(r.power + trajectory[i + 1], 0, 4) if r.fuel > 0 else 0
+        # except:
+        #     log(i, len(trajectory))
         i += 2
+
+        # times = np.append(times, datetime.now().microsecond)
 
         # Acceleration
         theta = math.radians(rotate + 90)
         ax = power * math.cos(theta)
         ay = power * math.sin(theta) - 3.711
 
+        # times = np.append(times, datetime.now().microsecond)
+
         # Next speed
         vx = r.vx + ax
         vy = r.vy + ay
+
+        # times = np.append(times, datetime.now().microsecond)
 
         # Next position
         x = r.x + r.vx + ax/2
@@ -94,15 +108,30 @@ def simulate_trajectory(rover: Rover, surface: Surface, trajectory: list[int]) \
         fuel = r.fuel - power
         if fuel < 0: fuel = 0
 
+        # times = np.append(times, datetime.now().microsecond)
+
         # Next rover
         prev_r = r
         r = Rover(x, y, vx, vy, fuel, rotate, power)
 
+        # times = np.append(times, datetime.now().microsecond)
+        # diff = np.diff(times)
+
+        # if initialized_times:
+        #     total_times = total_times + diff
+        # else:
+        #     total_times = diff
+        #     initialized_times = True
+
+    
     if on_surface:
         # place it on surface
         m, b = get_surface_line_below_rover(prev_r, surface)
         r.x = prev_r.x
         r.y = m * prev_r.x + b
+
+    # log(total_times)
+    # log(datetime.now().microsecond - start_sim)
 
     return r
 
@@ -147,22 +176,30 @@ def generate_population(pop_size, chrom_size) -> List[Chromosome]:
     return [create_chromosome(chrom_size) for i in range(pop_size)]
 
 def get_best_genetics(rover: Rover, surface: Surface):
-    start_time = datetime.now()
+    ga_start = datetime.now().microsecond
+
     pop_size = 10
     chrom_size = 30
     population = generate_population(pop_size, chrom_size)
 
     i = 0
+    best_chrom_idx = None
     winner = None
-    while not winner:
+    while not winner and (datetime.now().microsecond - ga_start) < 80000:
+        # times = np.append([], datetime.now().microsecond) ### 1
+
         # Normalize population scoring metrics
         pop_metrics = np.array([score_chrom(rover, surface, c) for c in population])
         pop_norm = preprocessing.minmax_scale(pop_metrics)
+
+        # times = np.append(times, datetime.now().microsecond) ### 2
 
         # By goals
         reached_lz = pop_metrics[:,0] < 1 # dist
         within_speed = reached_lz & (pop_metrics[:,1] == 0) # overspeed
         correct_rotation = within_speed & (pop_metrics[:,2] == 0) # rotation
+
+        # times = np.append(times, datetime.now().microsecond) ### 3
 
         # Add scores conditionally
         pop_scores = pop_norm[:,0]
@@ -170,18 +207,46 @@ def get_best_genetics(rover: Rover, surface: Surface):
         pop_scores += np.where(within_speed, pop_norm[:,2], 0)
         pop_scores += np.where(correct_rotation, pop_norm[:,3], 0)
 
+        # times = np.append(times, datetime.now().microsecond) ### 4
+
         # Sort by score
         sorted_population_idx = pop_scores.argsort()
-        best_i = sorted_population_idx[-1]
-        if correct_rotation[best_i]:
-            winner = population[best_i]
+        best_chrom_idx = sorted_population_idx[-1]
+        if correct_rotation[best_chrom_idx]:
+            winner = population[best_chrom_idx]
+            break
+
+        # Select best + random
+        perf_size = int(len(population) * GA_PERFORMANCE_PROPORTION)
+        select = [population[i] for i in sorted_population_idx[-perf_size:]]
         
+        rand_size = int(len(population) * GA_RANDOM_PROPORTION)
+        rand_chrom_indexes = random.choices(sorted_population_idx[:perf_size + 1], k = rand_size)
+        select += [population[i] for i in rand_chrom_indexes]
+        
+        # Populate with children
+        while len(select) < pop_size:
+            p1, p2 = random.choices(select, k = 2)
+            half_point = chrom_size // 2
+            half_point -= half_point % 2
+
+            child = p1[:half_point] + p2[half_point:]
+            mutation_idx = int(1 / GA_MUTATION_CHANCE * random.random())
+            if mutation_idx < chrom_size: 
+                mutation_idx -= mutation_idx % 2
+                child[mutation_idx] = random.choice(CHROM_ROTATIONS)
+                child[mutation_idx + 1] = random.choice(CHROM_POWERS)
+
+            select.append(child)
+        
+        population = select
         i += 1
-        if i > 50: break
+        
+        # times = np.append(times, datetime.now().microsecond) ### 4
+        # log(np.diff(times))
 
-    log((datetime.now() - start_time).microseconds)
-
-    return winner
+    log(i)
+    return winner if winner else population[best_chrom_idx]
 
 
 surface = Surface([], (0, 0), (0, 0))
